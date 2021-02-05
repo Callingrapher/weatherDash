bbb9c39aeb1a3d4976a82de077a89a49c61a4064
Matthew Eggleston <MW_Eggleston@msn.com>
2021-02-04 20:57:09 -0500

    Got my aPI calls working in terminal.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,23 @@
-
-
 var cities = [];
-var APIkey = "633201ba31240b0caf7b4be97fac60a3";
+var APIKey = "633201ba31240b0caf7b4be97fac60a3";
 var lat;
 var long;
 var forecastIndex = 0;
 
-function weatherInfo(city){
+function weatherInfo(city) {
     clearField();
-
+    
     //Queries the weather site with the user input
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?id=524901&appid=" + city + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
     $.ajax({
         url: queryURL,
-        method: "Get"
-    }).then(function (response, lat, long)
+        method: "GET"
+    }).then(function (response) {
         var lat = response.coord.lat;
+        console.log(response)
         var long = response.coord.lon;
-        // console.log(city + long + " and " + lat)
+        console.log(city + long + " and " + lat)
         var cityTemp = Math.round(response.main.temp);
         var cityTempIcon = response.weather[0].icon;
         var cityTempIconURL = "http://openweathermap.org/img/w/" + cityTempIcon + ".png";
@@ -29,39 +28,70 @@ function weatherInfo(city){
 
         // API call for Longitude and latitude for UV Index Info 
         var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + CityLat + "&lon=" + CityLong;
-       
+        getForecastInfo(city);
 
-    $.ajax({
-        url: queryURL,
-        method: "Get"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.value)
+            $('#current')
+                .append($("<aside>")).html(city + " (" + today + ")" + "");
+        })
     })
-
-    $('#current')
-    .append($("<aside>")).html(city + " (" + today + ")" + ""
 }
-
-
 
 //Next function causes the saved cities tabs to appear
 
-function renderButtons(city){
-    var citySearch = city.charAt(0).toUpperCase() + city.substring(1);
+function renderButtons(city) {
+            var citySearch = city.charAt(0).toUpperCase() + city.substring(1);
 
-    f (cities.indexOf(city) === -1) {
+            if (cities.indexOf(city) === -1) {
 
-        $("#search-history").append($("<button>").addClass("past-city").attr("city-name", citySearch).text(citySearch))
-        cities.push(citySearch);
-    } 
+                $("#search-history").append($("<button>").addClass("past-city").attr("city-name", citySearch).text(citySearch))
+                cities.push(citySearch);
+            }
 
-    $(".past-city").unbind().on("click", function (e) { 
-        e.preventDefault();
-        clearField();
-        displayWeatherInfo($(this).attr("city-name"));
+            $(".past-city").unbind().on("click", function (e) {
+                e.preventDefault();
+                clearField();
+                displayWeatherInfo($(this).attr("city-name"));
+            })
+
+        }
+
+function clearField() {
+            $("#current").empty();
+            $(".five-day").empty();
+            $(".forecast-head").empty();
+        }
+
+$("#add-city").on("click", function (e) {
+            e.preventDefault();
+            var city = $("#city-input").val().trim();
+            // displayWeatherInfo(city);
+            weatherInfo(city);
+            renderButtons(city);
+            document.GetElementById("city-input").value = "";
+        });
+
+function getForecastInfo(searchValue) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i =0; i < response.list.length; i +=8){
+            //dynamic card to append the five day forecast
+            
+        }
+            // console.log(response)
     })
-
 }
 
-function clearField(){
-    $("#current").empty();
+
+
+for (var i =0; i < response.list.length; i +=8){
 
 }
